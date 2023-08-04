df51877a23953d53a1e223f0501e46633c88acca
Sam <samuelkynno@gmail.com>
2023-08-04 13:35:54 +0100

    change

---
+++ src/db/models/user.js
@@ -0,0 +1,10 @@
+const {DataTypes} = require("sequelize")
+const database = require("../connection")
+
+const User = database.define("user", {
+    username: {type:DataTypes.STRING, allowNull:false, unique:true},
+    email: {type:DataTypes.STRING, allowNull:false, unique:true},
+    password: {type:DataTypes.STRING, allowNull:false},
+})
+
+module.exports = User
