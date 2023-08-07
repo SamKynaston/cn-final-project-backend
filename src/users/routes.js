da29b8330e8a319cfac8e09cc6066a5663c42edb
Sam <samuelkynno@gmail.com>
2023-08-07 09:50:34 +0100

    added in middleware for user routes

--- src/users/routes.js
+++ src/users/routes.js
@@ -4,9 +4,10 @@ const userRouter = Router()
 
 
 const { registerUser, login, } = require("./controllers")
+const { hashPass, comparePass, tokenCheck } = require("../middleware/index")
 
-userRouter.post("/users/register", registerUser)
-userRouter.post("/users/login", login)
+userRouter.post("/users/register", hashPass, registerUser)
+userRouter.post("/users/login", comparePass, login)
 
 module.exports = userRouter
 
