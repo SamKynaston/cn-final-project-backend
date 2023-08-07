6786422fee1af459b93f90dd527dde7c457a335e
Sam <samuelkynno@gmail.com>
2023-08-07 13:57:17 +0100

    added in token generation when signing up

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -16,7 +16,8 @@ const registerUser = async (req, res) => {
 
         res.status(201).json({
             message: "successfully registered",
-            user: {username: req.body.username, email: req.body.email}
+            user: {username: req.body.username, email: req.body.email},
+            token: await jwt.sign({id: req.user.id}, process.env.SECRET)
         })
 
 
